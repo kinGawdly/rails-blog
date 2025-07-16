b55a5c2acd160648769ff546d66436c940076668
Cristian Lizama <cristian.lizama@karibu.cl>
2025-07-16 23:28:25 +0000

    create and read CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,22 @@ class ArticlesController < ApplicationController
         end
     
     def new 
+        @article = Article.new
+    end
+
+    def show
+        @article = Article.find(params[:id])
+        # Assuming you want to find the article by ID, not by name
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_path
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:name, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new'
+  post '/articles', to: 'articles#create'
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new'
+  get '/articles/:id', to: 'articles#show', as: 'article'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
