625b0e1183b4a9be0592bb882f8ac065a856dd84
Cristian Lizama <cristian.lizama@karibu.cl>
2025-07-18 03:20:35 +0000

    crud complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,31 @@ class ArticlesController < ApplicationController
         # Assuming you want to find the article by ID, not by name
     end
 
+    def edit
+        @article = Article.find(params[:id])
+        # Assuming you want to find the article by ID, not by name
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        redirect_to article_path(@article)
+        # Redirect to the show page of the updated article
+    end
+
+    
     def create
         @article = Article.new(article_params)
         @article.save
         redirect_to articles_path
     end
-
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+        # Redirect to the index page after deletion
+    end
     private
     def article_params
         params.require(:article).permit(:name, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  patch '/articles/:id', to: 'articles#update', as: 'update_article'
   post '/articles', to: 'articles#create'
+  delete '/articles/:id', to: 'articles#destroy', as: 'article_destroy'
   get '/articles', to: 'articles#index'
   get '/articles/new', to: 'articles#new'
   get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
